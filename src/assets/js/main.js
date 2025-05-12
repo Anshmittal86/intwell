a3c4af279d6ac4b0b6ecf0ae8dca2966f1420364
Ansh Mittal <anshmit657@gmail.com>
2025-05-12 17:26:20 +0530

    ADD: Gsap Smooth Scroll and Line animation at hero section

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -4,17 +4,20 @@ const servicesContainerEl = document.querySelector('#services-container');
 const mobileMenuIcon = document.querySelector('#mobile-menu');
 const navbarEl = document.querySelector('nav');
 
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother, SplitText);
+
 // Get Services
-fetch('/intwell.json')
-  .then((response) => response.json())
-  .then((data) => {
-    const services = data.services;
+async function getServices() {
+  fetch('/intwell.json')
+    .then((response) => response.json())
+    .then((data) => {
+      const services = data.services;
 
-    services.forEach((service) => {
-      const { id, image, service: serviceName, description } = service;
+      services.forEach((service) => {
+        const { id, image, service: serviceName, description } = service;
 
-      servicesContainerEl.innerHTML += `
-      <div class="w-full bg-white border border-gray-200 rounded-lg shadow-sm p-5">
+        servicesContainerEl.innerHTML += `
+      <div class="w-full bg-white border flex flex-col justify-center items-center border-gray-200 rounded-lg shadow-sm p-5 text-center">
             <img
               class="rounded-t-lg mb-4"
               src="${image}"
@@ -27,70 +30,119 @@ fetch('/intwell.json')
                 <h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
                   ${serviceName}
                 </h3>
-              <p class="mb-3 font-normal text-gray-700">
+              <p class="mb-3 font-normal text-gray-700 text-justify">
                 ${description}
               </p>
             </div>
           </div>
       `;
+      });
+    });
+
+  mobileMenuIcon.addEventListener('click', () => {
+    // mobileNavbarEl.classList.toggle('hidden');
+    navbarEl.classList.toggle('active');
+  });
+}
+// Validate form and submit
+function validateForm() {
+  document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('contact-form');
+    const submitButton = form.querySelector('button[type="submit"]');
+
+    // Create status message element
+    const statusMessage = document.createElement('p');
+    statusMessage.className = 'text-green-600 font-medium mt-4';
+    form.appendChild(statusMessage);
+
+    form.addEventListener('submit', function (event) {
+      event.preventDefault();
+
+      // Show loader / disable button
+      submitButton.disabled = true;
+      const originalText = submitButton.textContent;
+      submitButton.textContent = 'Sending...';
+      submitButton.classList.add('bg-purple-600', 'text-white', 'cursor-not-allowed');
+
+      console.log(this, import.meta.env.VITE_SERVICE_ID);
+
+      emailjs
+        .sendForm(
+          import.meta.env.VITE_SERVICE_ID,
+          import.meta.env.VITE_TEMPLATE_ID,
+          this,
+          import.meta.env.VITE_PUBLIC_API
+        )
+        .then(
+          (response) => {
+            form.reset();
+            statusMessage.textContent = 'Message sent successfully!';
+            statusMessage.classList.remove('text-red-600');
+            statusMessage.classList.add('text-green-600');
+          },
+          (error) => {
+            statusMessage.textContent = 'Failed to send message. Please try again.';
+            statusMessage.classList.remove('text-green-600');
+            statusMessage.classList.add('text-red-600');
+          }
+        )
+        .finally(() => {
+          submitButton.disabled = false;
+          submitButton.textContent = originalText;
+          submitButton.classList.remove('bg-purple-600', 'text-white', 'cursor-not-allowed');
+        });
+
+      setTimeout(() => {
+        statusMessage.textContent = '';
+      }, 5000);
     });
   });
+}
+// Smooth scroll
 
-mobileMenuIcon.addEventListener('click', () => {
-  // mobileNavbarEl.classList.toggle('hidden');
-  navbarEl.classList.toggle('active');
+function handleSmoothScroll() {
+  let smoother = ScrollSmoother.create({
+    wrapper: '#main',
+    content: '#content',
+    smooth: 1.2,
+    effects: true
+  });
+
+  // Handle Anchor links smooth scroll
+  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute('href'));
+      if (target) {
+        smoother.scrollTo(target, true); // true = animate
+      }
+    });
+  });
+}
+
+gsap.to('.up-arrow', {
+  y: -5,
+  repeat: -1,
+  yoyo: true,
+  duration: 0.4
 });
 
-// Contact Form
-
-document.addEventListener('DOMContentLoaded', () => {
-  const form = document.getElementById('contact-form');
-  const submitButton = form.querySelector('button[type="submit"]');
-
-  // Create status message element
-  const statusMessage = document.createElement('p');
-  statusMessage.className = 'text-green-600 font-medium mt-4';
-  form.appendChild(statusMessage);
-
-  form.addEventListener('submit', function (event) {
-    event.preventDefault();
-
-    // Show loader / disable button
-    submitButton.disabled = true;
-    const originalText = submitButton.textContent;
-    submitButton.textContent = 'Sending...';
-    submitButton.classList.add('bg-purple-600', 'text-white', 'cursor-not-allowed');
-
-    console.log(this, import.meta.env.VITE_SERVICE_ID);
-
-    emailjs
-      .sendForm(
-        import.meta.env.VITE_SERVICE_ID,
-        import.meta.env.VITE_TEMPLATE_ID,
-        this,
-        import.meta.env.VITE_PUBLIC_API
-      )
-      .then(
-        (response) => {
-          form.reset();
-          statusMessage.textContent = 'Message sent successfully!';
-          statusMessage.classList.remove('text-red-600');
-          statusMessage.classList.add('text-green-600');
-        },
-        (error) => {
-          statusMessage.textContent = 'Failed to send message. Please try again.';
-          statusMessage.classList.remove('text-green-600');
-          statusMessage.classList.add('text-red-600');
-        }
-      )
-      .finally(() => {
-        submitButton.disabled = false;
-        submitButton.textContent = originalText;
-        submitButton.classList.remove('bg-purple-600', 'text-white', 'cursor-not-allowed');
-      });
-
-    setTimeout(() => {
-      statusMessage.textContent = '';
-    }, 5000);
+gsap.utils.toArray('.trigger-line-animation').forEach((triggerEl) => {
+  gsap.from(triggerEl.querySelectorAll('.line-animation'), {
+    scrollTrigger: {
+      trigger: triggerEl,
+      start: 'top 90%', // customize as needed
+      toggleActions: 'play none none none'
+    },
+    rotationX: -100,
+    transformOrigin: '50% 50% -160px',
+    opacity: 0,
+    duration: 1,
+    ease: 'power3',
+    stagger: 0.25
   });
 });
+
+handleSmoothScroll();
+getServices();
+validateForm();
